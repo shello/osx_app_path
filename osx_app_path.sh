a6260d197aad4a0a3616899ab8c0cdf1d38dbce6
Filipe Rodrigues <shello@shello.org>
2015-04-15 19:17:07 -0700

    Ensure an argument is provided.
    Usage is also clarified to require at least one parameter.

--- osx_app_path.sh
+++ osx_app_path.sh
@@ -2,9 +2,9 @@
 # Based on http://apple.stackexchange.com/a/132394
 osx_app_path() {
   local usage app_name bundle_id app_path exitcode
-
-  usage="Usage: $0 [[-a] <app name>|-b <bundle identifier>]"
-
+ 
+  usage="Usage: $0 <[-a] <app name>|-b <bundle identifier>>"
+ 
   while getopts "a:b:h" opt; do
     case $opt in
       a ) app_name="$OPTARG" ;;
@@ -13,27 +13,32 @@ osx_app_path() {
       ? ) echo "$usage" >&2; return 1 ;;
     esac
   done
-
+ 
   # Shift the parsed options from getopts, leaving the rest starting at $1
   shift "$((OPTIND-1))"
   if [[ -z "$bundle_id" && -n "$1" ]]; then
     app_name="$1"
   fi
-
+ 
   if [[ -z "$bundle_id" && -n "$app_name" ]]; then
     bundle_id="$(osascript -e "id of app \"$app_name\"")"
   fi
 
+  if [[ -z "$bundle_id" ]]; then
+    echo "$usage" >&2
+    return 1
+  fi
+ 
   app_path="$(
     osascript -e 'tell application "Finder"' \
               -e "POSIX path of (application file id \"$bundle_id\" as alias)" \
               -e 'end tell')"
   exitcode=$?
-
+ 
   if [[ $exitcode -eq 0 ]]; then
     echo "${app_path%/}"
   fi
-
+ 
   return $exitcode
 }
 
